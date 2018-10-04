cdb88745999765a669a264a3d09a96b6babef3a4
Muralidharraok <43461932+Muralidharraok@users.noreply.github.com>
2018-10-04 09:45:36 +0530

    2nd commit

--- src/prototype1/Prototype1.java
+++ src/prototype1/Prototype1.java
@@ -9,15 +9,17 @@ import javax.swing.border.MatteBorder;
 import java.util.*;
 import prototype1.Client2;
 import prototype1.Deal;
+import java.sql.*;
 
 
 
 public class Prototype1{
     JFrame jf;
-    JTabbedPane jt;
-    
-
-    public Prototype1() //extends JFrame
+    JTabbedPane jt; 
+    static Connection con;
+    static Statement stm;
+    static ResultSet rs;
+     public Prototype1() throws SQLException
     {
         
          jf=new JFrame("Demo!");
@@ -30,54 +32,27 @@ public class Prototype1{
          jt.addTab("DEAL",new Deal());
          jt.addTab("CLIENT",new Client2());
          jf.add(jt);
+         try
+         {
+             Class.forName("com.mysql.jdbc.Driver");
+             con=DriverManager.getConnection("jdbc:musql://localhost/agency","root","");
+         }
+         catch(ClassNotFoundException e)
+          {
+              System.out.println("Unable to load the driver");
+          }
+         catch(SQLException e)
+         {
+             System.out.println("Connection not established");
+         }
+         stm=con.createStatement();
     }
-    public static void main(String[] args) {
-        new Prototype1();
+    public static void main(String[] args) throws SQLException{
+        Prototype1 p=new Prototype1();
     }   
 }
 
-/*class Client extends JPanel implements ActionListener 
-{
-    JLabel l,l1,l2,l3,l4;
-    JTextField t,t1,t2,t3;
-    JButton b;
-   Client()
-    {
-        //setLayout(new MigLayout("ins 15"));
-        
-        l=new JLabel("NAME:");
-        t=new JTextField(10);
-        l1=new JLabel("DOB:");
-        t1=new JTextField(10);
-        l2=new JLabel("ADDRESS:");
-        t2=new JTextField(10);
-        l3=new JLabel("PHONE NO:");
-        t3=new JTextField(10);
-        b=new JButton("SUBMIT");
-        b.addActionListener(this);
-        l4=new JLabel();
-        //l4.setLocation(50,100);
-        l4.setText(" FILL ALL DETAILS CORRECTLY ");
-       
-        add(l);
-        add(t, "pushx, growx, wrap");
-        add(l1);
-        add(t1, "pushx, growx, wrap");
-        add(l2);
-        add(t2, "pushx, growx, wrap");
-        add(l3);
-        add(t3, "pushx, growx, wrap");
-        add(l4);
-        add(b,"span 2, split 2, center, gaptop 15");
-       // pack();
-    }
-   public void actionPerformed(ActionEvent ae)
-   {
-    l4.setText("SUCCESSFULLY UPLOADED TO DATABASE !!!");
-   }
-}*/
-
-/*public*/ class TitlePane extends JPanel {
+ class TitlePane extends JPanel {
 
         public TitlePane() {
             setLayout(new BorderLayout());            
@@ -87,16 +62,8 @@ public class Prototype1{
             label.setFont(new Font("Lucida Sans Unicode", Font.ITALIC, 50));
             label.setForeground(Color.YELLOW);
             setBackground(Color.black);
-             //Date date = new Date();
             setLayout(new BorderLayout());            
             setBorder(new CompoundBorder(new EmptyBorder(10,10, 10,10), new MatteBorder(1, 1, 2, 1, Color.GREEN)));
-            //JLabel labe2 = new JLabel();
-            
-            
-            
-            //setTitlePane().setBackground(Color.black);
-
-            //label.setFont(label.getFont().deriveFont(Font.Color='RED'));
             add(label);
         } 
          
